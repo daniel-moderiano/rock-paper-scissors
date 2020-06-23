3e05b0d5c4685c8ea1aa887e6aad529387d991b9
Daniel <daniel.moderiano@outlook.com>
2020-06-23 18:01:18 +0930

    Added support for full web app UI and addressed problem with round scoring with draws

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+        // computerPlay - function to randomly return either "Rock", "Paper" or "Scissors"
+     
+        function selectComputerChoice() {
+            let rand = Math.random();
+            if (rand <= 1/3) {
+                return "rock";
+            } else if (rand > 1/3 && rand <= 2/3) {
+                return "paper";
+            } else {
+                return "scissors";
+            }
+        }
+
+        function selectPlayerChoice() {
+            let playerSelection = window.prompt("Rock, Paper, or Scissors?");
+            return playerSelection.toLowerCase();
+        }
+        
+
+        function playRound(playerSelection, computerSelection) {
+            console.log((`You chose: ${playerSelection} \nThe computer chose: ${computerSelection}`));
+
+            if (playerSelection === computerSelection) {
+                return "It's a draw!";
+            }
+
+            if (playerSelection === "rock" && computerSelection === "paper") {
+                return "You lose! Paper beats Rock";
+            } else if (playerSelection === "rock" && computerSelection === "scissors") {
+                return "You win! Rock beats Scissors";
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
+                return "You lose! Scissors beats Paper";
+            } else if (playerSelection === "paper" && computerSelection === "rock") {
+                return "You win! Paper beats Rock";
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
+                return "You lose! Rock beats Scissors";
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
+                return "You win! Scissors beats Paper";
+            }
+        }
+        
+
+        function game() {
+            let roundCount = 1;
+            let compScore = 0;
+            let playerScore = 0;
+            for (i = 0; i < 5; i++) {
+                console.log(`ROUND ${roundCount}`);
+                let result = playRound(selectPlayerChoice(), selectComputerChoice());
+                if (result.includes("You win")) {
+                    playerScore += 1;
+                } else if (result.includes("draw")) {
+                    // pass
+                } else {
+                    compScore +=1;
+                }
+                roundCount += 1;
+                console.log(result + `\nSCORE \nYou: ${playerScore} \nComputer: ${compScore}`);
+            }
+            if (compScore > playerScore) {
+                return "Computer wins! Bad luck.";
+            } else if (compScore === playerScore) {
+                return "A complete draw!";
+            } else {
+                return "You win! Well done.";
+            }
+        }
